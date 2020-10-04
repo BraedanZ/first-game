76462570a8dbccea6c6274d5e4f3f5e49e505043
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2020-10-03 20:07:39 -0400

    Winnable

---
+++ ExperimentationWorld/Assets/Scripts/Winner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winner : MonoBehaviour {
+
+    Player playerScript;
+
+    void Start() {
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void OnTriggerEnter2D(Collider2D hitObject) {
+        if (hitObject.tag == "Player") {
+            playerScript.Win();
+        }
+    }
+}
